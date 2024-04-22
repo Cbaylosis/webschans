ad8fc55cfd1acb5cdc5552b062acda658018cadd
Christian C. Baylosis <nap.cbaylosis@gmail.com>
2024-04-23 00:17:13 +0800

    modify and delete code

--- forms/contact.php
+++ forms/contact.php
@@ -1,45 +1,41 @@
 <?php
-/**
- * Requires the "PHP Email Form" library
- * The "PHP Email Form" library is available only in the pro version of the template
- * The library should be uploaded to: vendor/php-email-form/php-email-form.php
- * For more info and help: https://bootstrapmade.com/php-email-form/
- */
 
-// Path to the php-email-form.php file
-$email_form_path = '../assets/vendor/php-email-form/php-email-form.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-// Check if the file exists
-if (file_exists($email_form_path)) {
-  require $email_form_path;
-} else {
-  die('Unable to load the "PHP Email Form" Library!');
+require 'vendor/autoload.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  // Instantiate PHPMailer
+  $mail = new PHPMailer(true);
+
+  try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'nap.cbaylosis@gmail.com';
+    $mail->Password = 'NAPbaylosis2024';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    //Recipients
+    $mail->setFrom('nap.cbaylosis@gmail.com', 'Christian');
+    $mail->addAddress('christiancbaylosis@gmail.com');
+
+    // Content
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
+
+    $mail->send();
+    echo 'Message has been sent';
+  } catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
 }
-
-// Replace contact@example.com with your real receiving email address
-$receiving_email_address = 'christiancbaylosis@gmail.com';
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
-
-// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-
-$contact->smtp = array(
-  'host' => 'smtp.gmail.com',
-  'username' => 'nap.cbaylosis@gmail.com',
-  'password' => 'pass',
-  'port' => '587'
-);
-
-
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
-?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '78ec28d24f2396d46de034be2114e753e133eb4f',
+        'reference' => 'ae2415394755ecedaa2eb749f38f4f694ba81e32',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '78ec28d24f2396d46de034be2114e753e133eb4f',
+            'reference' => 'ae2415394755ecedaa2eb749f38f4f694ba81e32',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
