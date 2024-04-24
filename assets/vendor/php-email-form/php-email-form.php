0757c1cf0403e6b8c1287734945b5aee677f5299
Christian C. Baylosis <nap.cbaylosis@gmail.com>
2024-04-24 23:33:37 +0800

    add sent message

--- assets/vendor/php-email-form/php-email-form.php
+++ assets/vendor/php-email-form/php-email-form.php
@@ -1,1 +1,35 @@
-    
+<?php
+
+class PHP_Email_Form
+{
+    public $ajax = false;
+    public $to = '';
+    public $from_name = '';
+    public $from_email = '';
+    public $subject = '';
+    public $message = '';
+
+    public function add_message($message, $label, $maxlength)
+    {
+        // Truncate message if it exceeds the maximum length
+        if (strlen($message) > $maxlength) {
+            $message = substr($message, 0, $maxlength);
+        }
+
+        // Add message content to the email body
+        $this->message .= "$label: $message\n";
+    }
+
+
+    public function send()
+    {
+        // Send the email using the configured settings
+        $headers = "From: $this->from_name <$this->from_email>\r\n";
+        $headers .= "Reply-To: $this->from_email\r\n";
+        $headers .= "MIME-Version: 1.0\r\n";
+        $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+        // Use PHP's mail() function to send the email
+        return mail($this->to, $this->subject, $this->message, $headers);
+    }
+}
--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,40 @@
 <?php
+/**
+ * Requires the "PHP Email Form" library
+ * The "PHP Email Form" library is available only in the pro version of the template
+ * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+ * For more info and help: https://bootstrapmade.com/php-email-form/
+ */
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'nap.cbaylosis@gmail.com';
 
-require 'vendor/autoload.php';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-
-  // Instantiate PHPMailer
-  $mail = new PHPMailer(true);
-
-  try {
-    //Server settings
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'nap.cbaylosis@gmail.com';
-    $mail->Password = 'NAPbaylosis2024';
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-
-    //Recipients
-    $mail->setFrom('nap.cbaylosis@gmail.com', 'Christian');
-    $mail->addAddress('christiancbaylosis@gmail.com');
-
-    // Content
-    $mail->isHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body = "Name: $name<br>Email: $email<br>Message: $message";
-
-    $mail->send();
-    echo 'Message has been sent';
-  } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-  }
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+  include ($php_email_form);
+} else {
+  die('Unable to load the "PHP Email Form" Library!');
 }
+
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
+
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
+$contact->smtp = array(
+'host' => 'example.com',
+'username' => 'example',
+'password' => 'pass',
+'port' => '587'
+);
+*/
+
+$contact->add_message($_POST['name'], 'From', 50);
+$contact->add_message($_POST['email'], 'Email', 50);
+$contact->add_message($_POST['message'], 'Message', 255);
+
+echo $contact->send();
